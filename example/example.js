832736708fddf642c5de150a38acab6c8eb096d2
David Halls <dahalls@gmail.com>
2017-09-22 23:16:33 +0100

    Fix example

--- example/example.js
+++ example/example.js
@@ -2,29 +2,17 @@ const fs = require('fs'),
       assert = require('assert'),
       Getdents = require('getdents').Getdents;
 
-fs.open('/tmp', 'r', function (err, fd)
+fs.open('/tmp', 'r', async function (err, fd)
 {
     assert.ifError(err);
 
     let getdents = new Getdents(1024 * 1024, fd);
 
-    getdents.more(function more(err, ended)
+    for await (let _ of getdents)
     {
-        assert.ifError(err);
-        
-        if (ended)
+        if (getdents.type() === Getdents.DT_REG)
         {
-            return fs.close(this.fd);
+            console.log(getdents.name());
         }
-
-        for (let _ of getdents)
-        {
-            if (getdents.type() === Getdents.DT_REG)
-            {
-                console.log(getdents.name());
-            }
-        }
-
-        this.more(more);
-    });
+    }
 });
